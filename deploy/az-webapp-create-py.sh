b2c1a4cd783ee3c8736af6e8d3838c7daef9786b
Christer Ljung <cljung@microsoft.com>
2019-05-15 14:49:43 +0200

    upd

--- deploy/az-webapp-create-py.sh
+++ deploy/az-webapp-create-py.sh
@@ -18,8 +18,25 @@ do
             AZAPPPLAN="$2"
             shift 2
             ;;
+        -t|--tenantid)
+            AZTENANTID="$2"
+            shift 2
+            ;;
+        -a|--appid)
+            AZAPPID="$2"
+            shift 2
+            ;;
+        -z|--zipfolder)
+            ZIPFOLDER="$2"
+            shift 2
+            ;;
     esac
 done
+
+echo "RG=$AZRGNAME, LOCATION=$AZLOCATION, PLAN=$AZAPPPLAN, APP=$AZAPPNAME, ZIPFOLDER=$ZIPFOLDER, tenantid=$AZTENANTID, AppID=$AZAPPID"
+
+rm -f ./deploy.zip
+zip -r ./deploy.zip $ZIPFOLDER
 
 # create the RG if it doesn't exists
 if [ $(az group exists -n $AZRGNAME) == 'false' ]; then
@@ -31,18 +48,16 @@ fi
 VAR0=$(az appservice plan show -n "$AZAPPPLAN" -g "$AZRGNAME" --query "name" -o tsv)
 if [ -z "$VAR0" ]; then
     echo "create AppPlan=$AZAPPPLAN"
-    az appservice plan create -n "$AZAPPPLAN" -g "$AZRGNAME" -l "$AZLOCATION" --is-linux --sku FREE #S1
+    az appservice plan create -g "$AZRGNAME" -n "$AZAPPPLAN" -l "$AZLOCATION" --is-linux --sku FREE #S1
 fi
 
 # create the AppService if it doesn't exists
 VAR0=$(az webapp show -n "$AZAPPNAME" -g "$AZRGNAME" --query "defaultHostName" -o tsv )
 if [ -z "$VAR0" ]; then
     echo "create AppService=$AZAPPNAME"
-    az webapp create -n "$AZAPPNAME" -g "$AZRGNAME" -p "$AZAPPPLAN" -r "python|3.7" \
-                    --deployment-local-git
-    az webapp config set -g $AZRGNAME -n $AZAPPNAME \
-                    --min-tls-version 1.2 \
-                    --linux-fx-version "PYTHON|3.7"
-    az webapp log config -n "$AZAPPNAME" -g "$AZRGNAME" --web-server-logging filesystem --docker-container-logging filesystem
+    az webapp create -g "$AZRGNAME" -p "$AZAPPPLAN" -n "$AZAPPNAME" -r "python|3.7" 
+    az webapp config set -g "$AZRGNAME" -n "$AZAPPNAME" --min-tls-version 1.2 --linux-fx-version "PYTHON|3.7"
+    az webapp log config -g "$AZRGNAME" -n "$AZAPPNAME" --web-server-logging filesystem --docker-container-logging filesystem
 fi
-
+az webapp config appsettings set -g "$AZRGNAME" -n "$AZAPPNAME" --settings "SCM_DO_BUILD_DURING_DEPLOYMENT=true" "AZTENANTID=$AZTENANTID" "AZAPPID=$AZAPPID"
+az webapp deployment source config-zip -g "$AZRGNAME" -n "$AZAPPNAME" --src ./deploy.zip
